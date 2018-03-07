b9912233bf61ef76f60cc64b2b6713c8eeaf1de7
mauroloon <mauricio.quezadagarcia@gmail.com>
2018-03-07 14:24:14 -0300

    Viernes 19-01

---
+++ app/src/main/java/com/letchile/let/Servicios/TransferirFotoFallida.java
@@ -0,0 +1,22 @@
+package com.letchile.let.Servicios;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by LETCHILE on 07/03/2018.
+ */
+
+public class TransferirFotoFallida extends Service{
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    //asdasdasd
+
+
+}
